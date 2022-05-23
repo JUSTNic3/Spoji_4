e99c3335f14140013f5712ca586d2c237588b32c
Rubinich <alojzrubinic@gmail.com>
2022-05-23 17:25:19 +0200

    Pregled izrade programa

--- Spoji_4.cpp
+++ Spoji_4.cpp
@@ -6,3 +6,17 @@ int main()
     cout << "Hello World!\n";
 }
 
+/*
+-poseban bin file ili odabir korisnika za pravila igre
+-suèelje sa odabirom poèetka igre,pravila,imena igraèa
+-21 eton za pojedinog igraèa
+-max 2 igraèa
+-znakovi na ploèi
+-2d polje
+-igraèa ploèa
+-funkcija za provjeru 4 iste boje paralelno,okomito i dijagonalno
+-spremanje rezultata u bin file (dodatna moguænost:sortiranje rezultata prema broju pobjeda ili kronološki)
+
+*ultra izazov(dodatan +):moguænost prestanka igre u bilo kojem trenutku te spremanje svega u bin file za kasniji nastavak
+
+*/
