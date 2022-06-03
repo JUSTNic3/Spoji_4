40c6f257ad768a6cea273191ff4bee4189b626b7
Rubinich <alojzrubinic@gmail.com>
2022-06-03 20:37:18 +0200

    dodatno sređivanje ploče (funkcija) , sljedece je upis simbola u bolje

--- Spoji_4.cpp
+++ Spoji_4.cpp
@@ -11,20 +11,27 @@ struct PlayerInfo {
     char PlayerID;
 };
 
-void show_grid(char ploca[6][8])
+void show_grid(char grid[6][8])
 {
-    cout << "\t        ";
+    cout << "\t ___   ___                 ___  ___ _____" << endl;
+    cout << "\t|     |   | |'   | |'   | |    |      |          /|" << endl;
+    cout << "\t|     |   | | '  | | '  | |__  |      |         / |" << endl;
+    cout << "\t|     |   | |  ' | |  ' | |    |      |        /__|_" << endl;
+    cout << "\t|___  |___| |   '| |   '| |___ |___   |           |" << endl;
+    cout << "       _____________________________________________________" << endl;
+    cout << endl;
+    cout << "\t           ";
     for (int i = 0; i < 8; i++)
     {
-        cout << i+1 << "   ";
+        cout << i + 1 << "   ";
     }
     cout << endl;
     for (int i = 0; i < 6; i++)
     {
-        cout << "\t       ";
+        cout << "\t          ";
         for (int j = 0; j < 8; j++)
         {
-            cout << "[" << ploca[i][j] << "] ";
+            cout << "[" << grid[i][j] << "] ";
         }
         cout << endl;
     }
@@ -73,27 +80,21 @@ int main()
         }
         else if (izbor == '2') {
             system("cls");
-            //unos imena igraca
             PlayerInfo player1, player2;
-            //upis imena i provjera je li string prazan
+
             cout << "ODABIR IMENA IGRACA" << endl << endl;
             cout << "Unesite ime 1. igraca: ";
             getline(cin, player1.PlayerName);
             cout << "Unesite ime 2. igraca: ";
             getline(cin, player2.PlayerName);
+
             player1.PlayerID = 'X';
             player2.PlayerID = 'O';
             cout << "Zeton 1. igraca ce biti: " << "\033[32m" << player1.PlayerID << "\033[0m" << endl;
             cout << "Zeton 2. igraca ce biti: " << "\033[32m" << player2.PlayerID << "\033[0m" << endl;
+
             _getch();
             system("cls");
-            cout << "\t ___   ___                 ___  ___ _____" << endl;
-            cout << "\t|     |   | |'   | |'   | |    |      |          /|" << endl;
-            cout << "\t|     |   | | '  | | '  | |__  |      |         / |" << endl;
-            cout << "\t|     |   | |  ' | |  ' | |    |      |        /__|_" << endl;
-            cout << "\t|___  |___| |   '| |   '| |___ |___   |           |" << endl;
-            cout << "       _____________________________________________________" << endl;
-            cout << endl;
             char grid[6][8];
             for (int i = 0; i < 6; i++)
             {
