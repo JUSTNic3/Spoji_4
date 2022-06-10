27450b00fe2ef16c310f32266f9a5f6cf736d163
Rubinich <alojzrubinic@gmail.com>
2022-06-10 17:15:17 +0200

    priprema za save & load

--- Spoji_4.cpp
+++ Spoji_4.cpp
@@ -1,8 +1,8 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
 #include <windows.h>
+#include <algorithm>
 using namespace std;
 
 struct PlayerInfo {
@@ -51,51 +51,40 @@ int PlayerDrop(char grid[6][8], PlayerInfo ActivePlayer)
         cin >> DropChoice;
         if (DropChoice > 0 && DropChoice < 9)
         {
-            while (grid[0][DropChoice - 1] != ' ') 
+            while (grid[0][DropChoice - 1] != ' ')
             {
                 cout << "Taj redak je pun , molim unesite novi redak: ";
                 cin >> DropChoice;
             }
         }
         else
-            cout << "\033[31m" <<  "Krivi stupac! Pokusajte ponovno." <<  "\033[0m" << endl;
+            cout << "\033[31m" << "Krivi stupac! Pokusajte ponovno." << "\033[0m" << endl;
     } while (DropChoice < 1 || DropChoice > 8);
-    return DropChoice; 
+    return DropChoice;
 }
 
 void CheckBellow(char grid[6][8], PlayerInfo ActivePlayer, int DropChoice)
 {
     int lgt = 5, t = 0;
     do {
-        if (grid[lgt][DropChoice-1] == ' ')
+        if (grid[lgt][DropChoice - 1] == ' ')
         {
-            grid[lgt][DropChoice-1] = ActivePlayer.PlayerID;
+            grid[lgt][DropChoice - 1] = ActivePlayer.PlayerID;
             t = 1;
         }
         else
-        
+
             --lgt;
     } while (t != 1);
 }
 
-int FullGrid(char grid[6][8])
-{
-    int full = 0;
-    for (int i = 0; i < 8; ++i)
-    {
-        if (grid[1][i] != ' ')
-            ++full;
-    }
-    return full;
-}
-
-int Check4(char grid[6][8], PlayerInfo ActivePlayer, int win)
+int Check4(char grid[5][8], PlayerInfo ActivePlayer, int win)
 {
     char znak = ActivePlayer.PlayerID;
     win = 0;
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < 7; j++)
         {
             if (grid[i][j] == znak && grid[i + 1][j + 1] == znak && grid[i + 2][j + 2] == znak && grid[i + 3][j + 3] == znak)
                 win = 1;
@@ -103,24 +92,21 @@ int Check4(char grid[6][8], PlayerInfo ActivePlayer, int win)
                 win = 1;
             if (grid[i][j] == znak && grid[i + 1][j] == znak && grid[i + 2][j] == znak && grid[i + 3][j] == znak)
                 win = 1;
-            if (grid[i][j] == znak && grid[i + 1][j - 1] == znak && grid[i + 2][j - 2] == znak && grid[i + 3][j - 3] == znak)
-                win = 1;
-            if (grid[i][j] == znak && grid[i][j - 1] == znak && grid[i][j - 2] == znak && grid[i][j - 3] == znak)
-                win = 1;
+            if(i < 2 && j > 3)
+                if (grid[i][j] == znak && grid[i + 1][j - 1] == znak && grid[i + 2][j - 2] == znak && grid[i + 3][j - 3] == znak)
+                    win = 1;
+            if(j > 2)
+                if (grid[i][j] == znak && grid[i][j - 1] == znak && grid[i][j - 2] == znak && grid[i][j - 3] == znak)
+                    win = 1;
         }
     }
     return win;
 }
 
-void PlayerWin(PlayerInfo ActivePlayer)
-{
-    cout << endl << "\033[32m" << ActivePlayer.PlayerName << ", pobijedio si!"  << "\033[0m" << endl;
-}
-
 int restart(char grid[6][8])
 {
     int restart = 0;
-    do{
+    do {
         cout << "Zelite li zaigrati ponovno? Da(1) Ne(2)" << endl;
         cin >> restart;
         if (restart == 1)
@@ -151,23 +137,41 @@ int restart(char grid[6][8])
     return restart;
 }
 
+int FullGrid(char grid[6][8])
+{
+    int full = 0;
+    for (int i = 0; i < 8; ++i)
+    {
+        if (grid[1][i] != ' ')
+            ++full;
+    }
+    return full;
+}
+
+void PlayerWin(PlayerInfo ActivePlayer)
+{
+    cout << endl << "\033[32m" << ActivePlayer.PlayerName << ", pobijedio si!" << "\033[0m" << endl;
+}
+
 bool nameCheck(PlayerInfo player1, PlayerInfo player2)
 {
-	
-	player1.PlayerName.erase(remove(player1.PlayerName.begin(), player1.PlayerName.end(), ' '), player1.PlayerName.end());
-	player2.PlayerName.erase(remove(player2.PlayerName.begin(), player2.PlayerName.end(), ' '), player2.PlayerName.end());
+
+    player1.PlayerName.erase(remove(player1.PlayerName.begin(), player1.PlayerName.end(), ' '), player1.PlayerName.end());
+    player2.PlayerName.erase(remove(player2.PlayerName.begin(), player2.PlayerName.end(), ' '), player2.PlayerName.end());
 
     for_each(player1.PlayerName.begin(), player1.PlayerName.end(), [](char& c) {c = tolower(c); });
-	for_each(player2.PlayerName.begin(), player2.PlayerName.end(), [](char& c) {c = tolower(c); });
-	if (player1.PlayerName == player2.PlayerName) {
-		cout << "\033[31m" <<"Ime je vec upotrebljeno! Pokusajte ponovmo!" << "\033[0m" << endl;
-		return true;
-	}
-	else {
-		cout << "\033[32m" << endl << "Ime 2. igraca (" << player2.PlayerName << ") uspjesno dodano. :)" << "\033[0m" << endl << endl;
-		return false;
-	}
+    for_each(player2.PlayerName.begin(), player2.PlayerName.end(), [](char& c) {c = tolower(c); });
+    if (player1.PlayerName == player2.PlayerName) {
+        cout << "\033[31m" << "Ime je vec upotrebljeno! Pokusajte ponovmo!" << "\033[0m" << endl;
+        return true;
+    }
+    else {
+        cout << "\033[32m" << endl << "Ime 2. igraca (" << player2.PlayerName << ") uspjesno dodano. :)" << "\033[0m" << endl << endl;
+        return false;
+    }
 }
+
+
 
 int main()
 {
